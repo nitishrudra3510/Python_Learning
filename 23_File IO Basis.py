dd78929839794cb2eca955caaeb75d21ac93558e
Nitish Kumar <114504003+nitishrudra3510@users.noreply.github.com>
2024-06-28 16:41:46 +0530

    Commited

--- 23_File IO Basis.py
+++ 23_File IO Basis.py
@@ -1,4 +1,26 @@
 # File to basics.
+
+# file I/O in python
+
+#python can used to perform operations on a file. (read&write the data)
+
+# types of file
+#1. text files: .txt. docx. log tec.
+# Binary files: .mp4, mov, png, .jpeg tec..
+
+# open, read and close files
+#--------------------------------
+
+we have to open a file before reading or writing..
+
+f = open("file_name", "mode")
+
+# file name       # r: read mode
+sample.txt         # w : write mode..
+
+data = f.read()
+f.close()
+
 '''
 "r" - open file for reading - default mode
 "w" -open a file for writing
@@ -8,7 +30,79 @@
 "b" - binary mode
 "+" - read and write
 
+
 '''
+
+a = open("/Users/nitishkumar/Documents/Python Learning/File Handling/file.txt", "r")
+data = a.read()
+print(data)
+print(type(data))
+a.close()
+
+# readline() - read only one lines..
+# read = read entire line.
+
+a = open("/Users/nitishkumar/Documents/Python Learning/File Handling/file.txt", "r")
+data = a.readline()
+print(data)
+
+line2 = a.readline()
+print(line2)
+a.close()
+
+
+
+# writing to a file..
+f = open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "w")
+f.write("I want to learn Javascript tommorow. 123 ")
+f.close()
+
+
+# append:
+
+f = open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "a")
+f.write("\nnniissh jcscb  hcbjkscs ")
+f.close()
+
+
+
+f = open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "r+")
+f.write("niissh")
+print(f.read())
+f.close()
+
+
+#### 
+
+f = open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "w+")
+# f.write("niissh")
+print(f.read())
+f.write("nitish") # nitish
+f.close()
+
+
+# another method:
+
+with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "r") as f:
+    data = f.read()
+    print(data)
+
+
+with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "w") as f:
+    data = f.write("abc")
+    print(data)
+
+
+# deleting a file:
+usign the os from django.utils.translation import ugettext_lazy as _
+module (like a code libarary) is a file written by another programmers that generally has a functions we can use.
+
+
+import os
+os.remove("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt")
+
+
+###
 
 f = open("python.txt")
 content = f.read()
@@ -119,3 +213,35 @@ f.close()
 
 
 
+
+
+# question practice..
+with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "w") as f:
+    f.write("Hi everyone\n we are learning file I/O \n")
+    f.write("using java.\n I am nitish kuamr.. java")
+
+# repplace the word of java with python
+
+with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "r") as f:
+    data = f.read()
+    
+new_data = data.replace("java", "python")
+print(new_data)
+
+# override the datas
+with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "w") as f:
+    f.write(new_data)
+    
+    
+# it is exit or not "learning."
+def check_for_word():
+    world = "learning"
+    with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "w") as f:
+        data = f.read()
+        if(data.find(word) != -1):
+            print("Found")
+      
+        else:
+            print("Not found!..") 
+            
+check_for_word()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,34 @@
 
-# code:
-integer = -20
-print("Absolurte value of -40 is: ", abs(integer))
+# it is exit or not "learning."
+def check_for_word():
+    word = "learning"
+    with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "w") as f:
+        data = f.read()
+        if(data.find(word) != -1):
+            print("Found")
+      
+        else:
+            print("Not found!..") 
+            
+check_for_word()
 
-# floating number:
-floating_number = -10.232
-print("absolute value -10.232 : ", abs(floating_number))
+
+
+def check_for_line():
+    word = "learning"
+    data = True
+    line_no = 1
+    with open("/Users/nitishkumar/Documents/GitHub/Python_Learning/nitish.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no +=1
+            
+            
+    return -1
+
+check_for_line()
+
+            
